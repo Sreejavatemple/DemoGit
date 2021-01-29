289c0216ade76ab6828b49893e8d6fc6ce474688
Srikanth <ksri66518@gmail.com>
2021-01-29 15:43:24 +0530

    Demo git code commited in fit account

---
+++ src/main/java/com/sree/Demo.java
@@ -0,0 +1,13 @@
+package com.sree;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class Demo implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("WEL come to Git Account");
+	}
+
+}
